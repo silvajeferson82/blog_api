802975bde98c4312bf6b4c97c2f4531b662176df
silvajeferson82 <jefersonsilva.contato@gmail.com>
2021-11-28 23:56:31 -0300

    Autenticação e config controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  #Permite configurar update de usuario 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :confirmable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  has_many :articles, dependent: :destroy
+
+  before_validation :set_uid
+
+  private
+
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
+end
